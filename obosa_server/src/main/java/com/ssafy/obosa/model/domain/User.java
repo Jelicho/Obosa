7fe538f103d978d4c16295d17ead67b78e778e21
ryudd <ryusoohan@naver.com>
2019-10-17 16:20:28 +0900

    refactor(User): add user name, nickname

--- obosa_server/src/main/java/com/ssafy/obosa/model/domain/User.java
+++ obosa_server/src/main/java/com/ssafy/obosa/model/domain/User.java
@@ -19,6 +19,8 @@ public class User extends DateEntity
     private String password;
     private int salt;
     private String email;
+    private String name;
+    private String nickname;
     private String address;
     private String zipCode;
     private String phone;
