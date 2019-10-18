8cc5e1fc835fb4a73a4ec72ef1d3a63b90f43eb7
ming <mkl0819@naver.com>
2019-10-18 09:28:46 +0900

    feat : implement user

---
+++ obosa_front/src/view/logoutView.vue
@@ -0,0 +1,31 @@
+<template></template>
+
+<script>
+export default {
+  name: "Logout",
+  mounted: function() {
+    this.logout();
+  },
+  methods: {
+    logout: function() {
+      var scope = this;
+
+      this.$userService.logout(function() {
+        store.state.isSigned = false;
+        store.state.user.id = 0;
+        store.state.user.hasWallet = false;
+        sessionStorage.removeItem("state");
+        scope.$router.push("/");
+
+        alert("성공적으로 로그아웃 되었습니다.");
+      });
+      if (sessionStorage.state) {
+        sessionStorage.removeItem("state");
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
