e48d150c58a5fd3ef20ee5040c5481b6cfec6c6c
최재림 <chlwofla0720@naver.com>
2019-10-18 16:10:35 +0900

    S1P1351021-33 refactor(User) : add NotNull annotation

--- obosa_server/src/main/java/com/ssafy/obosa/model/domain/User.java
+++ obosa_server/src/main/java/com/ssafy/obosa/model/domain/User.java
@@ -2,6 +2,8 @@ package com.ssafy.obosa.model.domain;
 
 import com.ssafy.obosa.model.domain.auditing.DateEntity;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import javax.validation.constraints.NotNull;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -15,15 +17,34 @@ import javax.persistence.Id;
 public class User extends DateEntity
 {
     @Id
+    @NonNull
     private int uid;
+
+    @NonNull
     private String password;
+
+    @NonNull
     private int salt;
+
+    @NonNull
     private String email;
+
+    @NonNull
     private String name;
+
+    @NonNull
     private String nickname;
+
+    @NonNull
     private String address;
+
+    @NonNull
     private String zipCode;
+
+    @NonNull
     private String phone;
+
     private String profileImg;
+
     private Boolean State;
 }
