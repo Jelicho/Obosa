ae88006d64e92fe0ecf5ce38886dced8548e36d3
최재림 <chlwofla0720@naver.com>
2019-10-18 16:11:39 +0900

    S1P1351021-33 refactor(UserRepository) : fix a findById->findByUid

--- obosa_server/src/main/java/com/ssafy/obosa/repository/UserRepository.java
+++ obosa_server/src/main/java/com/ssafy/obosa/repository/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, String>
 {
     User findByEmailAndPassword(String email, String password);
-    Optional<User> findById(int id);
+    Optional<User> findByUid(int id);
     Optional<User> findByEmail(String email);
     User findFirstByNickname(String nickname);
     List<User> findByName(String name);
