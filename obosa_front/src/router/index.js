f8952165b98030abcba0b10e46c55a8ea8e5d9fd
ming <mkl0819@naver.com>
2019-10-21 17:57:17 +0900

    feat(router) : add auction router

--- obosa_front/src/router/index.js
+++ obosa_front/src/router/index.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import homeView       from '@/component/view/homeView'
-import loginView      from '@/component/view/loginView'
-import logoutView     from '@/component/view/logoutView'
-import registerView   from '@/component/view/registerView'
-import auctionView    from '@/component/view/auctionView'
-import mypageView     from '@/component/view/mypageView'
+import homeView from '@/component/view/homeView'
+import loginView from '@/component/view/loginView'
+import logoutView from '@/component/view/logoutView'
+import registerView from '@/component/view/registerView'
+import auctionView from '@/component/view/auctionView'
+import mypageView from '@/component/view/mypageView'
+
+import auctionList from '@/component/auction/auctionList'
+import auctionDetail from '@/component/auction/auctionDetail'
 
 Vue.use(Router)
 
@@ -32,12 +35,26 @@ export default new Router({
     {
       name: "auction",
       path: '/auction',
-      component: auctionView
+      component: auctionView,
+      children: [{
+          name: "auction.list",
+          path: ':sortby',
+          component: auctionList,
+          params: {
+            sortby: 'count'
+          }
+        },
+        {
+          name: 'auction.detail',
+          path: 'detail/:auction',
+          component: auctionDetail,
+        }
+      ]
     },
     {
       name: "mypage",
       path: '/mypage',
-      component : mypageView
+      component: mypageView
     }
   ]
 })
