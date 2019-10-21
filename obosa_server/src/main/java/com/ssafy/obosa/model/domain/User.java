2fff6ab3cfb5b110081506befb233f03652c4788
ryudd <ryusoohan@naver.com>
2019-10-21 14:10:57 +0900

    S1P1351021-32 refactor(User) : add withDraw for ban users

--- obosa_server/src/main/java/com/ssafy/obosa/model/domain/User.java
+++ obosa_server/src/main/java/com/ssafy/obosa/model/domain/User.java
@@ -2,10 +2,12 @@ package com.ssafy.obosa.model.domain;
 
 import com.ssafy.obosa.model.domain.auditing.DateEntity;
 import com.ssafy.obosa.model.dto.SignupFormDto;
+import com.ssafy.obosa.util.AES256Util;
 import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.io.UnsupportedEncodingException;
 import java.util.Random;
 
 @Entity
@@ -27,7 +29,60 @@ public class User extends DateEntity
     private String zipCode;
     private String phone;
     private String profileImg;
+    private boolean withDraw = false;
     private boolean state;
+
+    public String getDecodedName()
+    {
+        try
+        {
+            AES256Util aes256Util = new AES256Util("171895718919871248975123589");
+            return aes256Util.aesDecoding(this.name);
+        }
+        catch (Exception e)
+        {
+            return "DECODE_ERROR";
+        }
+    }
+
+    public String getDecodedPhone()
+    {
+        try
+        {
+            AES256Util aes256Util = new AES256Util("171895718919871248975123589");
+            return aes256Util.aesDecoding(this.phone);
+        }
+        catch (Exception e)
+        {
+            return "DECODE_ERROR";
+        }
+    }
+
+    public String getDecodedZipCode()
+    {
+        try
+        {
+            AES256Util aes256Util = new AES256Util("171895718919871248975123589");
+            return aes256Util.aesDecoding(this.zipCode);
+        }
+        catch (Exception e)
+        {
+            return "DECODE_ERROR";
+        }
+    }
+
+    public  String getDecodedAddress()
+    {
+        try
+        {
+            AES256Util aes256Util = new AES256Util("171895718919871248975123589");
+            return aes256Util.aesDecoding(this.address);
+        }
+        catch (Exception e)
+        {
+            return "DECODE_ERROR";
+        }
+    }
 
     public static User setUserBySignupDto(SignupFormDto signupFormDto)
     {
