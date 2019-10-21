1d238be67f2403ff8a6ccd44986bd67732a4c2a6
ryudd <ryusoohan@naver.com>
2019-10-21 14:16:58 +0900

    S1P1351021-34 feat : one-way encryption implemented with SHA256 applied to AuthService

--- obosa_server/src/main/java/com/ssafy/obosa/service/common/AuthService.java
+++ obosa_server/src/main/java/com/ssafy/obosa/service/common/AuthService.java
@@ -3,6 +3,7 @@ package com.ssafy.obosa.service.common;
 import com.ssafy.obosa.model.common.LoginReq;
 import com.ssafy.obosa.model.domain.User;
 import com.ssafy.obosa.repository.UserRepository;
+import com.ssafy.obosa.util.SHA256Util;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,10 +18,17 @@ public class AuthService
 
     public boolean loginCheck(LoginReq loginReq)
     {
-        final User user = userRepository.findByEmailAndPassword(loginReq.getEmail(), loginReq.getPassword());
+        final User user = userRepository.findUserByEmail(loginReq.getEmail());
         if(user != null)
         {
-            return true;
+            SHA256Util sha256Util = new SHA256Util();
+
+            String hashedPw = sha256Util.SHA256Util(loginReq.getPassword()+user.getSalt());
+            if(hashedPw.equals(user.getPassword()))
+            {
+                return true;
+            }
+            return false;
         }
         return false;
     }
