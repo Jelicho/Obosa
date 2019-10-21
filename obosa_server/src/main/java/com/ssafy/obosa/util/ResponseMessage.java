1205b06fd85f5c622eb15db475bdbfdb9e1f51eb
최재림 <chlwofla0720@naver.com>
2019-10-21 17:52:23 +0900

    S1P1351021-37 fix(Response Message) : add CREATED_PRODUCT

--- obosa_server/src/main/java/com/ssafy/obosa/util/ResponseMessage.java
+++ obosa_server/src/main/java/com/ssafy/obosa/util/ResponseMessage.java
@@ -25,4 +25,7 @@ public class ResponseMessage
     public static final String INTERNAL_SERVER_ERROR = "서버 내부 에러";
     public static final String DB_ERROR = "데이터베이스 에러";
     public static final String BAD_REQUEST = "올바르지 않은 요청";
+
+    //ProductRegistration
+    public static final String CREATED_PRODUCT = "물품 등록 성공";
 }
