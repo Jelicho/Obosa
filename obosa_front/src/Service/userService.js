57de6a98b9efbf1a13a6763faa731e5b5d43cbf8
ming <mkl0819@naver.com>
2019-10-22 10:55:44 +0900

    fix(register) : fix user register service

--- obosa_front/src/Service/userService.js
+++ obosa_front/src/Service/userService.js
@@ -1,49 +1,48 @@
-export default{
-    findById: function(id, done){
-        $.get(API_BASE_URL + "/api/members/" + id).done(done);
-    },
-    signUp: function(email, name, password, callback){
-        var body = {
-            "이메일": email,
-            "이름": name,
-            "비밀번호": password
-        }
-
-        $.ajax({
-            type: "POST",
-            url: API_BASE_URL + "/api/members",
-            data: JSON.stringify(body),
-            headers: { 'Content-Type': 'application/json' },
-            success: function(response){
-                callback(response);
-            }
-        });
-    },
-    login: function(email, password, callback, whenError){
-        var body = {
-            "이메일": email,
-            "비밀번호": password
-        }
-
-        $.ajax({
-            type: 'POST',
-            url: API_BASE_URL + "/api/members/login",
-            data: JSON.stringify(body),
-            headers: { 'Content-Type': 'application/json' },
-            success: callback,
-            error: whenError
-        });
-    },
-    logout: function(callback){
-        callback();
-    },
-    update: function(body, callback){
-        $.ajax({
-            type: 'PUT',
-            url: API_BASE_URL + "/api/members",
-            data: JSON.stringify(body),
-            headers: { 'Content-Type': 'application/json' },
-            success: callback
-        })
+export default {
+  findById: function (id, done) {
+    $.get(API_BASE_URL + "/api/members/" + id).done(done);
+  },
+  signUp: function (regiFormData) {
+    $.ajax({
+      type: "POST",
+      url: API_BASE_URL + "/signup",
+      data: regiFormData,
+      processData: false,
+      contentType: false,
+      success: function (response) {
+        callback(response);
+      }
+    });
+  },
+  login: function (email, password, callback, whenError) {
+    var body = {
+      "이메일": email,
+      "비밀번호": password
     }
+
+    $.ajax({
+      type: 'POST',
+      url: API_BASE_URL + "/api/members/login",
+      data: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success: callback,
+      error: whenError
+    });
+  },
+  logout: function (callback) {
+    callback();
+  },
+  update: function (body, callback) {
+    $.ajax({
+      type: 'PUT',
+      url: API_BASE_URL + "/api/members",
+      data: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success: callback
+    })
+  }
 }
--- obosa_front/src/component/view/registerView.vue
+++ obosa_front/src/component/view/registerView.vue
@@ -1,163 +1,193 @@
 <template>
   <div>
-    <v-container class="container">
-      <div id="register-form" class="col-md-10 mx-auto bg-white">
-        <v-row>
-          <v-col sm="6" align="center">
-            <v-img :src="imgsrc(user.png)" width="300" height="300"></v-img>
-            <v-btn rounded depressed color="#FDD835" @click="upload()">
-              업로드
-              <input type="file" style="display:none" id="profile" accept=".gif, .jpg, .png" />
-            </v-btn>
-          </v-col>
-          <v-col sm="6" class="input-form">
-            <v-row>
-              <v-col>
-                <logo />
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col md="9">
-                <v-text-field
-                  full-width
-                  single-line
-                  label="이메일"
-                  v-model="user.email"
-                  background-color="#f4f8f7"
-                  color="grey darken-2"
-                  :rules="[rules.required, rules.email]"
-                ></v-text-field>
-              </v-col>
-              <v-col md="3" style="align-self:baseline!important">
-                <v-btn height="56px" width="100%" depressed color="error" error>인증</v-btn>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
-                <v-text-field
-                  full-width
-                  single-line
-                  label="닉네임"
-                  v-model="user.nickname"
-                  background-color="#f4f8f7"
-                  color="grey darken-2"
-                  :rules="[rules.required , rules.nickname]"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
-                <v-text-field
-                  full-width
-                  single-line
-                  type="password"
-                  label="비밀번호"
-                  v-model="user.password"
-                  background-color="#f4f8f7"
-                  color="grey darken-2"
-                  :rules="[rules.required, rules.password]"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
-                <v-text-field
-                  full-width
-                  single-line
-                  type="password"
-                  label="비밀번호 확인"
-                  v-model="user.passwordConfirm"
-                  background-color="#f4f8f7"
-                  color="grey darken-2"
-                  :rules="[rules.required, rules.pwConfirm]"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col @click="openapi()">
-                <v-text-field
-                  full-width
-                  single-line
-                  label="우편번호"
-                  v-model="user.zipcode"
-                  background-color="#f4f8f7"
-                  color="grey darken-2"
-                  disabled
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col @click="openapi()">
-                <v-text-field
-                  full-width
-                  single-line
-                  label="주소"
-                  v-model="user.address"
-                  background-color="#f4f8f7"
-                  color="grey darken-2"
-                  disabled
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <addressPopUp :dialog="addressDialog" @update:address="setAddress" />
-            <v-row>
-              <v-col sm="3">
-                <v-text-field
-                  full-width
-                  single-line
-                  label="010"
-                  v-model="user.phone.no1"
-                  background-color="#f4f8f7"
-                  color="grey darken-2"
-                  type="number"
-                  class="no-arrow"
-                ></v-text-field>
-              </v-col>
-              <v-col sm="1">-</v-col>
-              <v-col sm="3">
-                <v-text-field
-                  full-width
-                  single-line
-                  label="1234"
-                  v-model="user.phone.no2"
-                  background-color="#f4f8f7"
-                  color="grey darken-2"
-                  type="number"
-                  class="no-arrow"
-                ></v-text-field>
-              </v-col>
-              <v-col sm="1">-</v-col>
-              <v-col sm="3">
-                <v-text-field
-                  full-width
-                  single-line
-                  label="5678"
-                  v-model="user.phone.no3"
-                  background-color="#f4f8f7"
-                  color="grey darken-2"
-                  type="number"
-                  class="no-arrow"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
-                <v-btn
-                  block
-                  rounded
-                  large
-                  dark
-                  ripple
-                  color="teal"
-                  id="sign-up"
-                  @click="register()"
-                >회원가입</v-btn>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
-      </div>
-    </v-container>
+    <v-form ref="regiform" valid>
+      <v-container class="container">
+        <div id="register-form" class="col-md-10 mx-auto bg-white">
+          <v-row>
+            <v-col sm="6" align="center">
+              <v-img :src="imgsrc(user.png)" width="300" height="300"></v-img>
+              <v-btn rounded depressed color="#FDD835" @click="upload()">
+                업로드
+                <input type="file" style="display:none" id="profile" accept=".gif, .jpg, .png" />
+              </v-btn>
+            </v-col>
+            <v-col sm="6" class="input-form">
+              <v-row>
+                <v-col>
+                  <logo />
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col md="9">
+                  <v-text-field
+                    required
+                    full-width
+                    single-line
+                    label="이메일"
+                    v-model="user.email"
+                    background-color="#f4f8f7"
+                    color="grey darken-2"
+                    :rules="[rules.required, rules.email]"
+                  ></v-text-field>
+                </v-col>
+                <v-col md="3" style="align-self:baseline!important">
+                  <v-btn height="56px" width="100%" depressed color="error" error>인증</v-btn>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    required
+                    full-width
+                    single-line
+                    label="이름"
+                    v-model="user.name"
+                    background-color="#f4f8f7"
+                    color="grey darken-2"
+                    :rules="[rules.required]"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    required
+                    full-width
+                    single-line
+                    label="닉네임"
+                    v-model="user.nickname"
+                    background-color="#f4f8f7"
+                    color="grey darken-2"
+                    :rules="[rules.required , rules.nickname]"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    required
+                    full-width
+                    single-line
+                    type="password"
+                    label="비밀번호"
+                    v-model="user.password"
+                    background-color="#f4f8f7"
+                    color="grey darken-2"
+                    :rules="[rules.required, rules.password]"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    required
+                    full-width
+                    single-line
+                    type="password"
+                    label="비밀번호 확인"
+                    v-model="passwordConfirm"
+                    background-color="#f4f8f7"
+                    color="grey darken-2"
+                    :rules="[rules.required, rules.pwConfirm]"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col @click="openapi()">
+                  <v-text-field
+                    required
+                    full-width
+                    single-line
+                    label="우편번호"
+                    v-model="user.zipcode"
+                    background-color="#f4f8f7"
+                    color="grey darken-2"
+                    disabled
+                    :rules="[rules.required]"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col @click="openapi()">
+                  <v-text-field
+                    required
+                    full-width
+                    single-line
+                    label="주소"
+                    v-model="user.address"
+                    background-color="#f4f8f7"
+                    color="grey darken-2"
+                    disabled
+                    :rules="[rules.required]"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <addressPopUp :dialog="addressDialog" @update:address="setAddress" />
+              <v-row>
+                <v-col sm="3">
+                  <v-text-field
+                    required
+                    full-width
+                    single-line
+                    label="010"
+                    v-model="no1"
+                    :rules="[rules.required, rules.phone]"
+                    background-color="#f4f8f7"
+                    color="grey darken-2"
+                    type="number"
+                    class="no-arrow"
+                  ></v-text-field>
+                </v-col>
+                <v-col sm="1">-</v-col>
+                <v-col sm="3">
+                  <v-text-field
+                    required
+                    full-width
+                    single-line
+                    label="1234"
+                    v-model="no2"
+                    :rules="[rules.required, rules.phone]"
+                    background-color="#f4f8f7"
+                    color="grey darken-2"
+                    type="number"
+                    class="no-arrow"
+                  ></v-text-field>
+                </v-col>
+                <v-col sm="1">-</v-col>
+                <v-col sm="3">
+                  <v-text-field
+                    required
+                    full-width
+                    single-line
+                    label="5678"
+                    v-model="no3"
+                    :rules="[rules.required, rules.phone]"
+                    background-color="#f4f8f7"
+                    color="grey darken-2"
+                    type="number"
+                    class="no-arrow"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-btn
+                    block
+                    rounded
+                    large
+                    dark
+                    ripple
+                    color="teal"
+                    id="sign-up"
+                    @click="register()"
+                  >회원가입</v-btn>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </div>
+      </v-container>
+    </v-form>
   </div>
 </template>
 
@@ -168,20 +198,22 @@ export default {
   name: "Register",
   data() {
     return {
+      valid: true,
       addressDialog: false,
       user: {
         email: "",
+        name: "",
         nickname: "",
         password: "",
-        passwordConfirm: "",
         zipcode: "",
         address: "",
-        phone: {
-          no1: "",
-          no2: "",
-          no3: ""
-        }
+        phone: "",
+        profileImg: ""
       },
+      no1: "",
+      no2: "",
+      no3: "",
+      passwordConfirm: "",
       rules: {
         required: value => !!value || "입력해주세요",
         nickname: value =>
@@ -192,7 +224,8 @@ export default {
           "8자 이상, 12자 이하로 입력해주세요",
         email: value => /.+@.+\..+/.test(value) || "이메일 형식을 지켜주세요",
         pwConfirm: value =>
-          value == this.user.password || "비밀번호를 확인해주세요"
+          value == this.user.password || "비밀번호를 확인해주세요",
+        phone: value => value.length <= 4 || "전화번호를 정확히 입력해주세요."
       },
       message: ""
     };
@@ -208,21 +241,17 @@ export default {
   methods: {
     register: function() {
       var scope = this;
+      var regiFormData = new FormData();
+      this.user.phone = this.no1 + "-" + this.no2 + "-" + this.no3;
 
-      console.log(this.user);
-
-      if (this.user.password === this.user.passwordConfirm) {
-        this.$userService.signUp(
-          this.user.email,
-          this.user.name,
-          this.user.password,
-          function(response) {
-            alert("회원가입이 완료되었습니다.");
-            scope.$router.push("/");
-          }
-        );
+      if (this.$refs.regiform.validate()) {
+        for (var key in this.user) {
+          regiFormData.append(key, this.user[key]);
+        }
+        console.log("send data");
+        this.$userService.signUp(regiFormData)
       } else {
-        alert("비밀번호가 일치하지 않습니다.");
+        this.snackbar = true;
       }
     },
     imgsrc(src) {
