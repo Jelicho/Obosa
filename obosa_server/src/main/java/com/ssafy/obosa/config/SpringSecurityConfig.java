d5aeb6fbfb12ece901a65d631258341acca16849
mkl0819 <mkl0819@naver.com>
2019-10-24 01:45:49 +0900

    fix(register) : fix cors origin

--- obosa_server/src/main/java/com/ssafy/obosa/config/SpringSecurityConfig.java
+++ obosa_server/src/main/java/com/ssafy/obosa/config/SpringSecurityConfig.java
@@ -17,6 +17,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
+
         // comment out for develop
 //        http.csrf().disable().authorizeRequests()
 //                .antMatchers("/").permitAll()
