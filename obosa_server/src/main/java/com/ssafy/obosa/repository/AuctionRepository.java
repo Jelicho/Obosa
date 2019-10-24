882d1a80acd6a824405160f581baf4ea78cd291e
최재림 <chlwofla0720@naver.com>
2019-10-24 17:07:38 +0900

    S1P1351021-46 feat(Auction Repository) : add JPA query in repository to Auction table

--- obosa_server/src/main/java/com/ssafy/obosa/repository/AuctionRepository.java
+++ obosa_server/src/main/java/com/ssafy/obosa/repository/AuctionRepository.java
@@ -1,4 +1,14 @@
 package com.ssafy.obosa.repository;
 
-public interface AuctionRepository {
+import com.ssafy.obosa.model.domain.Auction;
+import com.ssafy.obosa.model.domain.Product;
+import com.ssafy.obosa.model.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AuctionRepository extends JpaRepository<Auction, String> {
+    Optional<User> findUserByAid(int aid);
+    Optional<Product> findProductByAid(int aid);
+    Optional<Auction> findAuctionByAid(int aid);
 }
