ebc3d88f36c8c9b361fe25844c5f9a68562479de
최재림 <chlwofla0720@naver.com>
2019-10-24 17:06:10 +0900

    S1P1351021-46 feat(Auction Update) : add Auction Update Dto and Service

--- obosa_server/src/main/java/com/ssafy/obosa/model/dto/UpdateAuctionDto.java
+++ obosa_server/src/main/java/com/ssafy/obosa/model/dto/UpdateAuctionDto.java
@@ -1,4 +1,20 @@
 package com.ssafy.obosa.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateAuctionDto {
+    private int aid;
+    private int lowPrice;
+    private int highPrice;
+    private String description;
+    private String endDate;
+    private char aucState;
+    private int upPrice;
 }
--- obosa_server/src/main/java/com/ssafy/obosa/service/UpdateAuctionService.java
+++ obosa_server/src/main/java/com/ssafy/obosa/service/UpdateAuctionService.java
@@ -1,4 +1,40 @@
 package com.ssafy.obosa.service;
 
+import com.ssafy.obosa.model.common.DefaultRes;
+import com.ssafy.obosa.model.domain.Auction;
+import com.ssafy.obosa.model.dto.UpdateAuctionDto;
+import com.ssafy.obosa.repository.AuctionRepository;
+import com.ssafy.obosa.repository.ProductRepository;
+import com.ssafy.obosa.repository.UserRepository;
+import com.ssafy.obosa.util.ResponseMessage;
+import com.ssafy.obosa.util.StatusCode;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UpdateAuctionService {
+    private final ProductRepository productRepository;
+    private final UserRepository userRepository;
+    private final AuctionRepository auctionRepository;
+
+    public UpdateAuctionService(final ProductRepository productRepository, final UserRepository userRepository, final AuctionRepository auctionRepository)
+    {
+        this.productRepository = productRepository;
+        this.userRepository=userRepository;
+        this.auctionRepository = auctionRepository;
+    }
+    public DefaultRes<UpdateAuctionDto> updateAuction(UpdateAuctionDto updateProductDto){
+        int aid = updateProductDto.getAid();
+        Optional<Auction> optionalAuction = auctionRepository.findAuctionByAid(aid);
+        if(!optionalAuction.isPresent()){
+            return DefaultRes.res(StatusCode.BAD_REQUEST, ResponseMessage.NOT_FOUND_AUCTION);
+        }
+
+        Auction auction = optionalAuction.get();
+        Auction.updateAuctionByAuctionDto(auction, updateProductDto);
+        auctionRepository.save(auction);
+
+        return DefaultRes.res(StatusCode.OK, ResponseMessage.UPDATED_AUCTION);
+    }
 }
