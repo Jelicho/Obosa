5ebee2b4c9ec9a00354d652e9d3f9afddebaad02
ANDREW <minjun0124@gmail.com>
2019-10-24 17:43:43 +0900

    (S1P1351021-35) feat(product) : making template

--- obosa_front/src/component/product/product.vue
+++ obosa_front/src/component/product/product.vue
@@ -1,13 +1,57 @@
 <template>
-  
+  <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    >
+      <v-card-title>상품명이 들어갑니다.</v-card-title>
+    </v-img>
+
+    <v-card-text class="text--primary">
+      <div>상품 설명</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="orange" text>
+        경매등록
+      </v-btn>
+
+      <v-btn color="orange" text>
+        상품삭제
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
+
 export default {
-name:'product'
+  name: 'product',
+  data() {
+    return {
+      dialog: false
+    }
+  },
+  props: {
+    pid: '',
+    pName: '',
+    pDesc: '',
+    pImg: [],
+  },
+  methods: {
+    deleteProduct(){
+      // TODO: 상품삭제 메소드 바인딩 params : pid
+    },
+    updatePost(){
+      // TODO: 상품삭제 메소드 바인딩 params : pid, pname, pdesc, pImg
+    },
+    auctionRegister(){
+      // TODO: quctionRegister binding(From. MK)
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
--- obosa_front/src/component/product/productList.vue
+++ obosa_front/src/component/product/productList.vue
@@ -1,11 +1,66 @@
-<template></template>
+<template>
+  <v-layout mt-5 wrap>
+    <v-flex v-for="i in products.length > limits ? limits : products.length" xs12 sm6 md4 lg3>
+      <product
+      :pId="products[i - 1].pId"
+      :pName="products[i - 1].pName"
+      :pDesc="products[i - 1].pDesc"
+      :pImg="products[i - 1].pImg"
+      ></product>
+      <v-divider></v-divider>
+    </v-flex>
+    <v-flex xs12 text-xs-center round my-5 v-if="loadMore">
+      <v-btn color="info" dark v-on:click="loadMorePosts"><v-icon size="25" class="mr-2">fa-plus</v-icon> 더 보기</v-btn>
+    </v-flex>
+  </v-layout>
+</template>
 
 <script>
 import product from "@/component/product/product";
+
 export default {
   name: "productList",
+  data() {
+    return products: []
+  },
   components: {
     product
+  },
+  methods: {
+    fetchProduct() {
+      this.$productService.findProductsByOwner(userId, function(productList) {
+        if (productList) {
+          this.products = product;
+        }
+      });
+    }
+  },
+  mounted: function() {
+    var scope = this;
+    var userId = this.sharedStates.user.id;
+
+    /**
+     * TODO 1. 회원의 작품 목록을 가져옵니다.
+     * Backend와 API 연동합니다.
+     * 작품 마다 소유권 이력을 보여줄 수 있어야 합니다.
+     */
+    // 여기에 작성하세요.
+    this.fetchproduct(userId);
+
+    /**
+     * TODO 2. 회원의 경매 목록을 가져옵니다.
+     * Backend와 API 연동합니다.
+     * 경매 중인 작품 마다 소유권 이력을 보여줄 수 있어야 합니다.
+     */
+    // 여기에 작성하세요.
+  },
+  computed: {
+    isExistproduct() {
+      if (this.products.length > 0) {
+        return true;
+      }
+      return false;
+    }
   }
 };
 </script>
