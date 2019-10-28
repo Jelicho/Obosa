e3435a30a01f79d6c45719a7cf8a318b72dc7526
mkl0819 <mkl0819@naver.com>
2019-10-28 11:21:41 +0900

    feat : implement loading animation and font-awesome setting

--- obosa_front/src/main.js
+++ obosa_front/src/main.js
@@ -22,10 +22,13 @@ Vue.prototype.$axios = axios
 import './assets/style.css'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 
-
-const opts = {theme: {themes: {light: {primary: '#3f51b5',secondary: '#b0bec5',accent: '#8c9eff',error: '#b71c1c',},},},}
+const opts = {
+    theme: { themes: { light: { primary: '#3f51b5', secondary: '#b0bec5', accent: '#8c9eff', error: '#b71c1c', }, }, },
+    icons: { iconfont: 'fa' }
+}
 
 Vue.use(Vuetify, opts);
 
