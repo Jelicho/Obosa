00e1c917ff670686426bcd7e97d07284481f41f8
mkl0819 <mkl0819@naver.com>
2019-10-29 14:24:34 +0900

    fix(auction) : fix cors error

--- obosa_server/src/main/java/com/ssafy/obosa/controller/AuctionController.java
+++ obosa_server/src/main/java/com/ssafy/obosa/controller/AuctionController.java
@@ -15,7 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 @RequestMapping("auction")
 public class AuctionController {
