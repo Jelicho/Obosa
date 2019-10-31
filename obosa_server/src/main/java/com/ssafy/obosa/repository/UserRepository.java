285a3b240f3b207f64f8da1586acaee37e58f746
최재림 <chlwofla0720@naver.com>
2019-10-31 15:44:53 +0900

    feat (User Query) : add a update User JPA S1P1351021-50

--- obosa_server/src/main/java/com/ssafy/obosa/repository/UserRepository.java
+++ obosa_server/src/main/java/com/ssafy/obosa/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.ssafy.obosa.repository;
 
 import com.ssafy.obosa.model.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +16,7 @@ public interface UserRepository extends JpaRepository<User, String>
     User findUserByEmail(String email);
     User findUserByNickname(String nickname);
     List<User> findByName(String name);
+    @Modifying
+    @Query("update USER U set U.withDraw = TRUE where U.uid = ?2")
+    int setFixedWithDrawFromUid(int uid);
 }
