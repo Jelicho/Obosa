a6ac80a8e617bdf096f7ad34d3fc498b376ac847
최재림 <chlwofla0720@naver.com>
2019-10-31 15:31:38 +0900

    feat (Scheduler frame) : add Scheduler frame TODO detail function S1P1351021-50

--- obosa_server/src/main/java/com/ssafy/obosa/util/Scheduler.java
+++ obosa_server/src/main/java/com/ssafy/obosa/util/Scheduler.java
@@ -15,13 +15,12 @@ public class Scheduler {
     }
     @Scheduled(cron = "0 0 * * * *")
     public void auctionClose() {
+        //enddate를 통해 끝난 경매 찾기=>aid 가져오기
+        //updateAuctionService에 존재하는 낙찰 method에 aid넘기기(낙찰)->insert winningbid
+
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date now = new Date();
         String nowDateTime = sdf.format(now);
         System.out.println("Java cron job expression:: " + nowDateTime);
-    }
-    @Scheduled(cron = "* * * * * *")
-    public void Test() {
-        deleteProductService.Test(14);
     }
 }
