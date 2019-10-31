9a95b1fcf3153a117ec41fdd90a7bf727f3cbaa0
최재림 <chlwofla0720@naver.com>
2019-10-31 15:43:50 +0900

    feat (Product Query) : add a delete Product JPA S1P1351021-50

--- obosa_server/src/main/java/com/ssafy/obosa/repository/ProductRepository.java
+++ obosa_server/src/main/java/com/ssafy/obosa/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.ssafy.obosa.repository;
 import com.ssafy.obosa.model.domain.Product;
 import com.ssafy.obosa.model.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +13,6 @@ public interface ProductRepository extends JpaRepository<Product, String>
     List<Product> findByUser(User user);
     List<Product> findByPnameContaining(String pname);
     Optional<Product> findByPid(int pid);
+    @Transactional
+    long deleteByPid(int pid);
 }
