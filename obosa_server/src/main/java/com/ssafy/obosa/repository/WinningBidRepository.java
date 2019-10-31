d75d4c915561768fa61ab157360192becc3ce05a
최재림 <chlwofla0720@naver.com>
2019-10-31 15:45:37 +0900

    feat (Winning Query) : add a select update delete WinningBid JPA S1P1351021-50

--- obosa_server/src/main/java/com/ssafy/obosa/repository/WinningBidRepository.java
+++ obosa_server/src/main/java/com/ssafy/obosa/repository/WinningBidRepository.java
@@ -1,4 +1,27 @@
 package com.ssafy.obosa.repository;
 
-public class WinningBidRepository {
+import com.ssafy.obosa.model.domain.User;
+import com.ssafy.obosa.model.domain.WinningBid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+public interface WinningBidRepository extends JpaRepository<WinningBid, String> {
+    Optional<WinningBid> findWinningBidByWid(int wid);
+    @Modifying
+    @Query("update WinningBid WB set WB.bidState = ?1 where WB.wid = ?2")
+    int setFixedBidStateFromWid(int bidState, int wid);
+    @Modifying
+    @Query("update WinningBid WB set WB.address = ?1 where WB.wid = ?2")
+    int setFixedAddressFromWid(String address, int wid);
+    @Transactional
+    long deleteByBid(int bid);
+
+    Page<WinningBid> findByUser(User user, Pageable pageable);
+    Page<WinningBid> findByAuction_User(User user, Pageable pageable);
 }
