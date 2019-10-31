01f9fbe45f19d111da289662d767a5c45cb59515
최재림 <chlwofla0720@naver.com>
2019-10-31 15:43:02 +0900

    feat (Auction Query) : add a update and delete Auction JPA S1P1351021-50

--- obosa_server/src/main/java/com/ssafy/obosa/repository/AuctionRepository.java
+++ obosa_server/src/main/java/com/ssafy/obosa/repository/AuctionRepository.java
@@ -4,6 +4,9 @@ import com.ssafy.obosa.model.domain.Auction;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -12,4 +15,9 @@ public interface AuctionRepository extends JpaRepository<Auction, String> {
     Page<Auction> findAll(Pageable pageable);
     Page<Auction> findByProduct_PnameContaining(String searchStr, Pageable pageable);
     Page<Auction> findByUser_NicknameContaining(String searchStr, Pageable pageable);
+    @Modifying
+    @Query("update Auction auc set auc.aucState = ?1 where auc.endDate <= ?2")
+    int setFixedAucStateFromNowDateTime(char aucState, String nowDateTime);
+    @Transactional
+    long deleteByAid(int aid);
 }
