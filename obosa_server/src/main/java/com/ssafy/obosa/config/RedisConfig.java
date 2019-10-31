70e47597c297bfd2103c45e7f11f31a1ee321e70
bbq923 <bbq9234@naver.com>
2019-10-31 12:18:56 +0900

    feat: modify redis config

--- obosa_server/src/main/java/com/ssafy/obosa/config/RedisConfig.java
+++ obosa_server/src/main/java/com/ssafy/obosa/config/RedisConfig.java
@@ -4,6 +4,7 @@ import com.ssafy.obosa.model.domain.Auction;
 import com.ssafy.obosa.model.domain.Product;
 import com.ssafy.obosa.model.domain.User;
 import com.ssafy.obosa.service.common.RedisService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -12,16 +13,24 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig
 {
+    @Value("${spring.redis.host}")
+    private String redisHostName;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory()
     {
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisHostName, redisPort);
         return lettuceConnectionFactory;
     }
 
