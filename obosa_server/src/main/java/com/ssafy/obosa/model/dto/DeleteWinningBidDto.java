4ce06364ca24472d0ade587c4504707bd64ff70d
최재림 <chlwofla0720@naver.com>
2019-10-31 15:30:30 +0900

    feat (WinningBid delete) : add delete WinningBid and Auction S1P1351021-50

--- obosa_server/src/main/java/com/ssafy/obosa/model/dto/DeleteWinningBidDto.java
+++ obosa_server/src/main/java/com/ssafy/obosa/model/dto/DeleteWinningBidDto.java
@@ -1,4 +1,14 @@
 package com.ssafy.obosa.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DeleteWinningBidDto {
+    private int wid;
 }
