ecf427fb6a362079b00a8caf4370328a4d105bfc
최재림 <chlwofla0720@naver.com>
2019-10-31 15:35:23 +0900

    feat (update WinningBid) : add update WinningBid Methods S1P1351021-50

--- obosa_server/src/main/java/com/ssafy/obosa/model/dto/UpdateWinningBidDto.java
+++ obosa_server/src/main/java/com/ssafy/obosa/model/dto/UpdateWinningBidDto.java
@@ -1,4 +1,19 @@
 package com.ssafy.obosa.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateWinningBidDto {
+    private int wid;
+    private String address;
+
+    public UpdateWinningBidDto(int wid) {
+        this.wid = wid;
+    }
 }
--- obosa_server/src/main/java/com/ssafy/obosa/service/UpdateWinningBidService.java
+++ obosa_server/src/main/java/com/ssafy/obosa/service/UpdateWinningBidService.java
@@ -1,4 +1,61 @@
 package com.ssafy.obosa.service;
 
+import com.ssafy.obosa.model.common.DefaultRes;
+import com.ssafy.obosa.model.domain.User;
+import com.ssafy.obosa.model.domain.WinningBid;
+import com.ssafy.obosa.model.dto.UpdateWinningBidDto;
+import com.ssafy.obosa.repository.UserRepository;
+import com.ssafy.obosa.repository.WinningBidRepository;
+import com.ssafy.obosa.util.ResponseMessage;
+import com.ssafy.obosa.util.StatusCode;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UpdateWinningBidService {
+    private final WinningBidRepository winningBidRepository;
+    private final UserRepository userRepository;
+    UpdateWinningBidService(final WinningBidRepository winningBidRepository, final UserRepository userRepository){
+        this.winningBidRepository = winningBidRepository;
+        this.userRepository = userRepository;
+    }
+
+    public DefaultRes<UpdateWinningBidDto> updateAddress(UpdateWinningBidDto updateWinningBidDto)
+    {
+        try{
+            int wid = updateWinningBidDto.getWid();
+            String address = updateWinningBidDto.getAddress();
+            winningBidRepository.setFixedAddressFromWid(address, wid);
+            return DefaultRes.res(StatusCode.OK, ResponseMessage.UPDATED_WINNINGBID_ADDRESS);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return DefaultRes.res(StatusCode.INTERNAL_SERVER_ERROR, ResponseMessage.INTERNAL_SERVER_ERROR);
+        }
+    }
+    public DefaultRes<UpdateWinningBidDto> updateWinningBidState(UpdateWinningBidDto updateWinningBidDto, int bidState)
+    {
+        try{
+            int wid = updateWinningBidDto.getWid();
+            Optional<WinningBid> optionalWinningBid = winningBidRepository.findWinningBidByWid(wid);
+            if(!optionalWinningBid.isPresent()){
+                return DefaultRes.res(StatusCode.BAD_REQUEST, ResponseMessage.NOT_FOUND_WINNINGBID);
+            }
+            winningBidRepository.setFixedBidStateFromWid(bidState, wid);
+            if(bidState == 4){
+                WinningBid winningBid = optionalWinningBid.get();
+                Optional<User> optionalUser = userRepository.findByUid(winningBid.getUser().getUid());
+                User user = optionalUser.get();
+                userRepository.setFixedWithDrawFromUid(user.getUid());
+            }
+            return DefaultRes.res(StatusCode.OK, ResponseMessage.UPDATED_WINNINGBID_STATE);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return DefaultRes.res(StatusCode.INTERNAL_SERVER_ERROR, ResponseMessage.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
