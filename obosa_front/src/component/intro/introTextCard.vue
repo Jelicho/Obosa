f99655fa358a9a1ae12cb590bdc22754378b9e20
mkl0819 <mkl0819@naver.com>
2019-11-01 18:32:00 +0900

    feat(intro) : add button click event

--- obosa_front/src/component/intro/introTextCard.vue
+++ obosa_front/src/component/intro/introTextCard.vue
@@ -6,7 +6,7 @@
         <br />경매에 참여할 수 있습니다.
       </p>
       <br />
-      <v-btn outlined tile large color="white">경매 참여하기</v-btn>
+      <v-btn outlined tile large color="white" @click="to('auction.list', 'count')">경매 참여하기</v-btn>
     </v-col>
     <v-col cols="6" class="intro-card intro-card-2">
       <p>
@@ -14,14 +14,22 @@
         <br />새로운 주인을 찾아주세요.
       </p>
       <br />
-      <v-btn outlined tile large color="white">회원가입</v-btn>
+      <v-btn outlined tile large color="white" @click="to('register')">회원가입</v-btn>
     </v-col>
   </v-row>
 </template>
 
 <script>
 export default {
-  name: "introTextCard"
+  name: "introTextCard",
+  methods: {
+    to(name, params) {
+      this.$router.push({
+        name: name,
+        params : { sortby : params || null }
+      });
+    }
+  }
 };
 </script>
 
