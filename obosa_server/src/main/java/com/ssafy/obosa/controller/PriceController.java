c986996cd8a3b737e27fb83ae1886f78a852232d
CHOHYEONIN <kasitw1124@gmail.com>
2019-11-04 10:58:55 +0900

    feat(priceController): add redis to get high price

--- obosa_server/src/main/java/com/ssafy/obosa/controller/PriceController.java
+++ obosa_server/src/main/java/com/ssafy/obosa/controller/PriceController.java
@@ -2,6 +2,7 @@ package com.ssafy.obosa.controller;
 
 import com.ssafy.obosa.model.dto.AuctionIdDto;
 import com.ssafy.obosa.model.dto.PriceDto;
+import com.ssafy.obosa.repository.BidRedisRepository;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -10,11 +11,17 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class PriceController 
 {
+    private final BidRedisRepository bidRedisRepository;
+    
+    PriceController(final BidRedisRepository bidRedisRepository) {
+        this.bidRedisRepository = bidRedisRepository;
+    }
     @MessageMapping("/price")
     @SendTo("/topic/price")
     public PriceDto getHighPrice(AuctionIdDto id) throws Exception
     {
         //TODO : redis에서 가격 가져오기
-        return new PriceDto(id.getId(), 1000);
+         int highestPrice = bidRedisRepository.findById(id.getId()).get().getHighestBid();
+        return new PriceDto(id.getId(), highestPrice);
     }
 }
