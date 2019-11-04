b2d4e15977c4abf772b63d0aed798989e6786243
mkl0819 <mkl0819@naver.com>
2019-11-04 15:56:30 +0900

    refactor(redis) : delete comments

--- obosa_server/src/main/java/com/ssafy/obosa/service/common/RedisService.java
+++ obosa_server/src/main/java/com/ssafy/obosa/service/common/RedisService.java
@@ -1,21 +1,21 @@
-// package com.ssafy.obosa.service.common;
+package com.ssafy.obosa.service.common;
 
-// import org.springframework.data.redis.connection.Message;
-// import org.springframework.data.redis.connection.MessageListener;
-// import org.springframework.stereotype.Service;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Service;
 
-// import java.util.ArrayList;
-// import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
-// @Service
-// public class RedisService implements MessageListener
-// {
-//     public static List<String> messageList = new ArrayList<>();
+@Service
+public class RedisService implements MessageListener
+{
+    public static List<String> messageList = new ArrayList<>();
 
-//     @Override
-//     public void onMessage(Message message, byte[] pattern)
-//     {
-//             messageList.add(message.toString());
-//             System.out.println("Message received : " + message.toString());
-//     }
-// }
+    @Override
+    public void onMessage(Message message, byte[] pattern)
+    {
+            messageList.add(message.toString());
+            System.out.println("Message received : " + message.toString());
+    }
+}
