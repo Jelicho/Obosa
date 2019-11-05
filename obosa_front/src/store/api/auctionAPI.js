1121de749c79d1b0e0180231af1ad5708705da29
bbq923 <bbq9234@naver.com>
2019-11-05 11:40:12 +0900

    feat(api_url): add url prefix api

--- obosa_front/src/store/api/auctionAPI.js
+++ obosa_front/src/store/api/auctionAPI.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 axios.defaults.headers.common['Authorization'] = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJvYm9zYSIsImV4cCI6MTU3MjI0MDY4OSwiZW1haWwiOiJzZEBzZC5zZCJ9.F7Mus15lPlEYXReVFJKMIwwhqlbfal3hPex_AZdyuOk";
-const apiUrl = 'http://localhost:8080/auction'
+// const apiUrl = 'http://localhost:8080/auction'
+const apiUrl = 'http://obosa.ssafy.io/api/auction'
 
 export default {
     getAuctionList(params) {
